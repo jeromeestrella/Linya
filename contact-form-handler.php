c9b7ea4fc4831b037528bc903600e8b1ff5f775b
jeromeestrella <44581878+jeromeestrella@users.noreply.github.com>
2018-11-02 20:02:19 +0800

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -26,7 +26,7 @@ $email_body = "You have received a new message. ".
 "Email: $email_address\n Message \n $message";
 $headers = "From: $myemail\n";
 $headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$message,$headers);
 //redirect to the 'thank you' page
 header('Location: https://linya.herokuapp.com');
 }
